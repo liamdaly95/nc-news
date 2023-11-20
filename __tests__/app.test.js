ec04f7ccecb4a8d46035ef735aff89a3e06cdf46
liamdaly95 <liamdaly95@gmail.com>
2023-11-20 15:06:54 +0000

    adds /api path, and testing, which responds with documentation

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
 const request = require("supertest");
 const { topicData, userData, articleData, commentData } = require("../db/data/test-data/index");
+const fs = require("fs/promises")
 
 beforeEach(() => seed({ topicData, userData, articleData, commentData }));
 afterAll(() => db.end());
@@ -34,3 +35,29 @@ describe("GET /api/topics", () => {
       });
   });
 });
+
+describe("GET /api", () => {
+  test('200: should respond with an object describing all the API endpoints', () => {
+    return request(app)
+    .get("/api")
+    .expect(200)
+    .then(({body}) => {
+      const readEndpointsPromise = fs.readFile(`${__dirname}/../endpoints.JSON`,"utf-8")
+      return Promise.all([body, readEndpointsPromise])
+    })
+    .then((array) => {
+      const body = array[0];
+      const contents = JSON.parse(array[1])
+      const numberOfPaths = Object.keys(contents).length
+      expect(Object.keys(body.endpoints)).toHaveLength(numberOfPaths)
+      for (const endpoint in body.endpoints) {
+        expect(body.endpoints[endpoint]).toMatchObject({
+          description: expect.any(String),
+          queries: expect.any(Array),
+          requestBodyFormat: expect.any(Object),
+          exampleResponse: expect.any(Object)
+        })
+      }
+    })
+  });
+})
--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,8 +1,15 @@
-const { selecTopics } = require("../models/models")
+const { selecTopics, readDocumentation } = require("../models/models")
 
 exports.getTopics = (req, res, next) => {
     selecTopics().then((topics) => {
         res.status(200).send({topics})
+    })
+    .catch(next)
+}
+
+exports.getDocumentation = (req, res, next) => {
+    readDocumentation().then((endpoints) => {
+        res.status(200).send({endpoints})
     })
     .catch(next)
 }
