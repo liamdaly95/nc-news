04037901bb725eaa91adeae98b00d0b5a964a5ed
liamdaly95 <liamdaly95@gmail.com>
2023-11-20 15:50:23 +0000

    simplifies testing by requiring in endpoints.JSON file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const seed = require("../db/seeds/seed");
 const request = require("supertest");
 const { topicData, userData, articleData, commentData } = require("../db/data/test-data/index");
 const fs = require("fs/promises")
+const endpoints = require("../endpoints.json")
 
 beforeEach(() => seed({ topicData, userData, articleData, commentData }));
 afterAll(() => db.end());
@@ -42,14 +43,8 @@ describe("GET /api", () => {
     .get("/api")
     .expect(200)
     .then(({body}) => {
-      const readEndpointsPromise = fs.readFile(`${__dirname}/../endpoints.JSON`,"utf-8")
-      return Promise.all([body, readEndpointsPromise])
-    })
-    .then((array) => {
-      const body = array[0];
-      const contents = JSON.parse(array[1])
-      const numberOfPaths = Object.keys(contents).length
-      expect(Object.keys(body.endpoints)).toHaveLength(numberOfPaths)
+      const numberOfRoutes = Object.keys(endpoints).length
+      expect(Object.keys(body.endpoints)).toHaveLength(numberOfRoutes)
       for (const endpoint in body.endpoints) {
         expect(body.endpoints[endpoint]).toMatchObject({
           description: expect.any(String),
