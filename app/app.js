448a92c606be0449e2cdabd5370444060eacefbd
liamdaly95 <liamdaly95@gmail.com>
2023-12-11 09:59:43 +0000

    allow cross origin resource sharing

--- app/app.js
+++ app/app.js
@@ -1,8 +1,10 @@
+const cors = require('cors');
 const express = require("express");
 const { handleServerErrors, handleCustomErrors, handlePsqlErrors } = require("../error-handling/error-handling");
 const apiRouter = require("../routes/api-router");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
